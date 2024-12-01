0a93abd4bce5d7e238612e4e1e12de083703eff8
TheLionCoder <eorlando.reyes@icloud.com>
2024-12-01 09:33:16 -0500

    remove unnused functions

--- utils.py
+++ utils.py
@@ -12,26 +12,6 @@ from typing import List, Generator
 
 import polars as pl
 
-
-def encode_categorical_features_by_frequency(
-    dataframe: pl.DataFrame, *, input_col: List[str] | str, normalized: bool = True
-) -> pl.DataFrame:
-    """Encode Categorical Features by frequency.
-    :param dataframe: polars.DataFrame
-    :param input_col: List[str]
-    :param normalized: bool
-    :return: polars.DataFrame
-    :raises: ValueError if input_col is not a list
-    e.g.
-      >>> "encode_categorical_features(dataframe=dataframe,input_col=['column_name', 'other_column_name'])"
-    """
-    encoded_column_name: str = f"{input_col}_ce"
-    total_rows: int = dataframe.height if normalized else 1
-    return dataframe.group_by(input_col).agg(
-        pl.len().truediv(pl.lit(total_rows)).alias(encoded_column_name)
-    )
-
-
 def read_file_chunks(file_path: Path) -> Generator[bytes, None, None]:
     """
     Read a file and yield its content in chunks
